d90c99222f7dcf7b3eab8372ee7a3f05d3fd20cd
unknown <2641574067@qq.com>
2020-09-09 18:49:16 +0800

    undefined ENAGLO

--- RinexReader/RinexReader.cpp
+++ RinexReader/RinexReader.cpp
@@ -464,54 +464,55 @@ void pntpos_test(){
 }
 
 
-int main(int argc, _TCHAR* argv[])
-{
-	printf("start...\n");
-	//utest2();
-	//beidoutest();
-	//beidousave();
-	//GPStest();
-	//GPSsave();
-	pntpos_test();
-	printf("finished\n");
-	return 0;
-}
-
-///* satpos estimation*/
-//int main(int argc, char**argv)
+//int main(int argc, _TCHAR* argv[])
 //{
-//	char file[] = "./data/GPS/SAVE2020-08-27_15-43-55.20N";
-//	nav_t nav = { 0 };
-//	double ep[] = { 2020, 8, 16, 0, 0, 0 };
-//	double rs[6] = { 0 }, dts[2] = { 0 };
-//	double var;
-//	gtime_t t, time;
-//	int svh = 0;
-//
-//	time = epoch2time(ep);
-//	readrnx(file, 1, "", NULL, &nav, NULL);
-//
-//	traceopen("satpos.trace");
-//	tracelevel(3);
-//
-//	for (int i = 0; i < 3600; i += 30)
-//	{
-//		t = timeadd(time, (double)i);
-//		double epoch[6] = { 0 };
-//		time2epoch(t, epoch);
-//		printf("%lf,%lf,%lf,%lf,%lf,%lf\n", epoch[0], epoch[1], epoch[2], epoch[3], epoch[4], epoch[5]);
-//
-//		for (int sat = 0; sat < MAXPRNGPS; sat++)
-//		{
-//			int ret = satpos(t, t, sat, EPHOPT_BRDC, &nav, rs, dts, &var, &svh);
-//			if (ret)//1:OK; 0:error
-//			{
-//				printf("%02d %6d %14.3f %14.3f %14.3f %14.3f\n",
-//					sat, i, rs[0], rs[1], rs[2], dts[0] * 1E9);
-//			}
-//		}
-//
-//	}
-//	traceclose();
+//	printf("start...\n");
+//	//utest2();
+//	//beidoutest();
+//	//beidousave();
+//	//GPStest();
+//	//GPSsave();
+//	pntpos_test();
+//	printf("finished\n");
 //	return 0;
 //}
+
+/* satpos estimation*/
+int main(int argc, char**argv)
+{
+	//char file[] = "./data/GPS/SAVE2020-08-27_15-43-55.20N";
+	char file[] = "./data/GPS/daej229a00.20n";
+	nav_t nav = { 0 };
+	double ep[] = { 2020, 8, 16, 0, 0, 0 };
+	double rs[6] = { 0 }, dts[2] = { 0 };
+	double var;
+	gtime_t t, time;
+	int svh = 0;
+
+	time = epoch2time(ep);
+	readrnx(file, 1, "", NULL, &nav, NULL);
+
+	traceopen("satpos.trace");
+	tracelevel(3);
+
+	for (int i = 0; i < 3600; i += 30)
+	{
+		t = timeadd(time, (double)i);
+		double epoch[6] = { 0 };
+		time2epoch(t, epoch);
+		printf("%lf,%lf,%lf,%lf,%lf,%lf\n", epoch[0], epoch[1], epoch[2], epoch[3], epoch[4], epoch[5]);
+
+		for (int sat = 0; sat < MAXPRNGPS; sat++)
+		{
+			int ret = satpos(t, t, sat, EPHOPT_BRDC, &nav, rs, dts, &var, &svh);
+			if (ret)//1:OK; 0:error
+			{
+				printf("%02d %6d %14.3f %14.3f %14.3f %14.3f\n",
+					sat, i, rs[0], rs[1], rs[2], dts[0] * 1E9);
+			}
+		}
+
+	}
+	traceclose();
+	return 0;
+}
